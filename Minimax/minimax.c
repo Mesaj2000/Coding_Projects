9d0bf4ec3df34a80631268f4a152e8ebca346dc6
Mesaj2000 <james@James-Simmons-Laptop.localdomain>
2018-12-02 18:19:33 -0500

    Commented Minimax

--- Minimax/minimax.c
+++ Minimax/minimax.c
@@ -59,15 +59,17 @@ int main(void)
 	destroyTree(top);
 }
 
+// Print the instructions to the game
 void instructions(void)
 {
-	printf("Welcome to TreeGame!\n");
-	printf("You and the computer will take turns bisecting the tree.\n");
+	printf("Welcome to Minimax!\n");
+	printf("You and the computer will take turns bisecting a set of numbers.\n");
 	printf("Your goal is to finish on the largest number possible.\n");
 	printf("The computer will try to finish on the lowest number possible.\n");
 	printf("Good Luck!\n");
 }
 
+// Print the score: the difference between the number obtained and the best possible number
 void printScore(node* current, node* top)
 {
 	printf("Your score was %d. (lower is better)\n", top->value - current->value);
@@ -82,6 +84,8 @@ void printScore(node* current, node* top)
 	}
 }
 
+// Prompt the user if they would like to play again
+// Return 1 if yes, 0 if no
 int playAgain(void)
 {
 	char* input = calloc(MAXLEN, sizeof(char));
@@ -99,6 +103,8 @@ int playAgain(void)
 	}
 }
 
+// Prompt the player if they would like to select the left or right side of the remaining numbers
+// Only evaluate the first character of their response, since that's all you need
 void getPlayerMove(node** this)
 {
 	char* input = calloc(MAXLEN, sizeof(char));
@@ -124,6 +130,8 @@ void getPlayerMove(node** this)
 	}
 }
 
+// The computer makes the ideal descision for any given state
+// Defaults to left if the value is identical
 void getCompMove(node** this)
 {
 	int chooseLeft = (*this)->value == (*this)->left->value;
@@ -131,6 +139,8 @@ void getCompMove(node** this)
 	*this = chooseLeft ? (*this)->left : (*this)->right;
 }
 
+// Fills the tree with random values at the bottom layer
+// and the ideal descisions at each other layer
 void fillTree(node* this, int layer)
 {
 	this->layer = layer;
@@ -156,6 +166,7 @@ void fillTree(node* this, int layer)
 		this->value = max(this->left->value, this->right->value);
 }
 
+// Print out the entire bottom layer of the tree
 void printTree(node* this)
 {
 	if (this->layer >= NUM_LAYERS)
@@ -172,6 +183,8 @@ void printTree(node* this)
 	printTree(this->right);
 }
 
+// Print out a valid path to the equilibrium position
+// Defaults left if multiple valid paths
 void printPath(node* this)
 {
 	if (this->layer >= NUM_LAYERS)
@@ -184,6 +197,7 @@ void printPath(node* this)
 	printPath(chooseLeft ? this->left : this->right);
 }
 
+// Free up all the memory in the tree
 void destroyTree(node* this)
 {	
 	if (this->left != NULL)
